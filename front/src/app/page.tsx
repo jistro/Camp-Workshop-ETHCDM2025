8b02cb7bf468cfde2f9d3f34ddbe5048138dcd32
jistro <contact@jistro.xyz>
2025-04-20 19:49:52 -0600

    agregado dependencias basicas

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -1,12 +1,27 @@
 // import { cookieStorage, createStorage, http } from '@wagmi/core'
+'use client';
 import { ConnectButton } from "@/components/ConnectButton";
 import { InfoList } from "@/components/InfoList";
 import { ActionButtonList } from "@/components/ActionButtonList";
 import Image from 'next/image';
+import ip from '../../ABI/ip.json';
+import { readContract } from "wagmi/actions";
+import { config } from "../config";
 
 const CONTRACT_ADDRESS = "0x725E60Adb60332D8C47d9FDA860ff34dD57ABA25";
 
 export default function Home() {
+  function getIpID() {
+    const ipID = document.getElementById("IpIDInput") as HTMLInputElement;
+    readContract(config, {
+      abi: ip,
+      address: CONTRACT_ADDRESS as `0x${string}`,
+      functionName: "getPropiedadIntelectual",
+      args: [ipID.value],
+    }).then((data: any) => {
+      console.log(data);
+    });
+  }
 
   return (
     <div className={"pages"}>
@@ -15,6 +30,10 @@ export default function Home() {
 
       <ConnectButton />
       <ActionButtonList />
+
+      <p>Obtener porpiedades de propiedad intelectual</p>
+      <input type="text" id="IpIDInput" placeholder="ej. 1256" /> 
+      <button onClick={getIpID}>Obtener</button>
       <div className="advice">
         <p>
           This projectId only works on localhost. <br/>Go to <a href="https://cloud.reown.com" target="_blank" className="link-button" rel="Reown Cloud">Reown Cloud</a> to get your own.
